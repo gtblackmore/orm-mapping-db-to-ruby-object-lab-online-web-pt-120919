e76fb4dc46e901017448dc410d79c63281133807
George Blackmore <gtblackmore@gmail.com>
2020-02-20 04:44:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade < '12'
+      WHERE grade != '12'
     SQL
     
     DB[:conn].execute(sql).map do |row|
@@ -72,7 +72,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT id
+      SELECT *
       FROM students
       WHERE grade = '10'
       LIMIT 1
