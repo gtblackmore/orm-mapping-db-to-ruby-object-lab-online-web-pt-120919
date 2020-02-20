8b7d2b21bfba4b016aee8f41764b9de43d63bd7d
George Blackmore <gtblackmore@gmail.com>
2020-02-20 04:41:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,8 +71,31 @@ class Student
   end
   
   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = '10'
+      ORDER BY name
+      LIMIT 1
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end
+  end
+    
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
